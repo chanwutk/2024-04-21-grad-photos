7d763f6227b94b33dc38ecf80d6689fd3fd33aef
Chanwut (Mick) Kittivorawong <30903997+chanwutk@users.noreply.github.com>
2024-04-30 17:24:29 +0000

    update layout

--- gen.py
+++ gen.py
@@ -6,6 +6,11 @@ def page(number: int):
   <meta charset="utf-8">
   <title>Thai Grad 2024: Graduation Photos</title>
 <style>
+  body {'{'}
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  {'}'}
   .navigation-container {'{'}
     display: flex;
     justify-content: center;
@@ -17,7 +22,9 @@ def page(number: int):
     font-size: 16px;
     cursor: pointer;
   {'}'}
-  img {'{'}
+  .a-img {'{'}
+    width: 30%;
+    height: auto;
     margin: 10px;
   {'}'}
 </style>
@@ -28,16 +35,16 @@ def page(number: int):
     <a href="http://chanwutk.github.io/2024-04-21-grad-photos/{(number+1):02d}" class="n-btn">next</a>
   </div>
   <div style="display: flex; flex-direction: row; flex-wrap: wrap;">
-    <a href="https://github.com/chanwutk/2024-04-21-grad-photos/blob/main/DSCF0{number:02d}0.JPG?raw=true"><img src="https://github.com/chanwutk/2024-04-21-grad-photos/blob/main/SMALL-DSCF0{number:02d}0.JPG?raw=true" alt="" width="45%" height="auto"></a>
-    <a href="https://github.com/chanwutk/2024-04-21-grad-photos/blob/main/DSCF0{number:02d}1.JPG?raw=true"><img src="https://github.com/chanwutk/2024-04-21-grad-photos/blob/main/SMALL-DSCF0{number:02d}1.JPG?raw=true" alt="" width="45%" height="auto"></a>
-    <a href="https://github.com/chanwutk/2024-04-21-grad-photos/blob/main/DSCF0{number:02d}2.JPG?raw=true"><img src="https://github.com/chanwutk/2024-04-21-grad-photos/blob/main/SMALL-DSCF0{number:02d}2.JPG?raw=true" alt="" width="45%" height="auto"></a>
-    <a href="https://github.com/chanwutk/2024-04-21-grad-photos/blob/main/DSCF0{number:02d}3.JPG?raw=true"><img src="https://github.com/chanwutk/2024-04-21-grad-photos/blob/main/SMALL-DSCF0{number:02d}3.JPG?raw=true" alt="" width="45%" height="auto"></a>
-    <a href="https://github.com/chanwutk/2024-04-21-grad-photos/blob/main/DSCF0{number:02d}4.JPG?raw=true"><img src="https://github.com/chanwutk/2024-04-21-grad-photos/blob/main/SMALL-DSCF0{number:02d}4.JPG?raw=true" alt="" width="45%" height="auto"></a>
-    <a href="https://github.com/chanwutk/2024-04-21-grad-photos/blob/main/DSCF0{number:02d}5.JPG?raw=true"><img src="https://github.com/chanwutk/2024-04-21-grad-photos/blob/main/SMALL-DSCF0{number:02d}5.JPG?raw=true" alt="" width="45%" height="auto"></a>
-    <a href="https://github.com/chanwutk/2024-04-21-grad-photos/blob/main/DSCF0{number:02d}6.JPG?raw=true"><img src="https://github.com/chanwutk/2024-04-21-grad-photos/blob/main/SMALL-DSCF0{number:02d}6.JPG?raw=true" alt="" width="45%" height="auto"></a>
-    <a href="https://github.com/chanwutk/2024-04-21-grad-photos/blob/main/DSCF0{number:02d}7.JPG?raw=true"><img src="https://github.com/chanwutk/2024-04-21-grad-photos/blob/main/SMALL-DSCF0{number:02d}7.JPG?raw=true" alt="" width="45%" height="auto"></a>
-    <a href="https://github.com/chanwutk/2024-04-21-grad-photos/blob/main/DSCF0{number:02d}8.JPG?raw=true"><img src="https://github.com/chanwutk/2024-04-21-grad-photos/blob/main/SMALL-DSCF0{number:02d}8.JPG?raw=true" alt="" width="45%" height="auto"></a>
-    <a href="https://github.com/chanwutk/2024-04-21-grad-photos/blob/main/DSCF0{number:02d}9.JPG?raw=true"><img src="https://github.com/chanwutk/2024-04-21-grad-photos/blob/main/SMALL-DSCF0{number:02d}9.JPG?raw=true" alt="" width="45%" height="auto"></a>
+    <a class="a-img" href="https://github.com/chanwutk/2024-04-21-grad-photos/blob/main/DSCF0{number:02d}0.JPG?raw=true"><img src="https://github.com/chanwutk/2024-04-21-grad-photos/blob/main/SMALL-DSCF0{number:02d}0.JPG?raw=true" width=100% height=auto alt=""></a>
+    <a class="a-img" href="https://github.com/chanwutk/2024-04-21-grad-photos/blob/main/DSCF0{number:02d}1.JPG?raw=true"><img src="https://github.com/chanwutk/2024-04-21-grad-photos/blob/main/SMALL-DSCF0{number:02d}1.JPG?raw=true" width=100% height=auto alt=""></a>
+    <a class="a-img" href="https://github.com/chanwutk/2024-04-21-grad-photos/blob/main/DSCF0{number:02d}2.JPG?raw=true"><img src="https://github.com/chanwutk/2024-04-21-grad-photos/blob/main/SMALL-DSCF0{number:02d}2.JPG?raw=true" width=100% height=auto alt=""></a>
+    <a class="a-img" href="https://github.com/chanwutk/2024-04-21-grad-photos/blob/main/DSCF0{number:02d}3.JPG?raw=true"><img src="https://github.com/chanwutk/2024-04-21-grad-photos/blob/main/SMALL-DSCF0{number:02d}3.JPG?raw=true" width=100% height=auto alt=""></a>
+    <a class="a-img" href="https://github.com/chanwutk/2024-04-21-grad-photos/blob/main/DSCF0{number:02d}4.JPG?raw=true"><img src="https://github.com/chanwutk/2024-04-21-grad-photos/blob/main/SMALL-DSCF0{number:02d}4.JPG?raw=true" width=100% height=auto alt=""></a>
+    <a class="a-img" href="https://github.com/chanwutk/2024-04-21-grad-photos/blob/main/DSCF0{number:02d}5.JPG?raw=true"><img src="https://github.com/chanwutk/2024-04-21-grad-photos/blob/main/SMALL-DSCF0{number:02d}5.JPG?raw=true" width=100% height=auto alt=""></a>
+    <a class="a-img" href="https://github.com/chanwutk/2024-04-21-grad-photos/blob/main/DSCF0{number:02d}6.JPG?raw=true"><img src="https://github.com/chanwutk/2024-04-21-grad-photos/blob/main/SMALL-DSCF0{number:02d}6.JPG?raw=true" width=100% height=auto alt=""></a>
+    <a class="a-img" href="https://github.com/chanwutk/2024-04-21-grad-photos/blob/main/DSCF0{number:02d}7.JPG?raw=true"><img src="https://github.com/chanwutk/2024-04-21-grad-photos/blob/main/SMALL-DSCF0{number:02d}7.JPG?raw=true" width=100% height=auto alt=""></a>
+    <a class="a-img" href="https://github.com/chanwutk/2024-04-21-grad-photos/blob/main/DSCF0{number:02d}8.JPG?raw=true"><img src="https://github.com/chanwutk/2024-04-21-grad-photos/blob/main/SMALL-DSCF0{number:02d}8.JPG?raw=true" width=100% height=auto alt=""></a>
+    <a class="a-img" href="https://github.com/chanwutk/2024-04-21-grad-photos/blob/main/DSCF0{number:02d}9.JPG?raw=true"><img src="https://github.com/chanwutk/2024-04-21-grad-photos/blob/main/SMALL-DSCF0{number:02d}9.JPG?raw=true" width=100% height=auto alt=""></a>
   </div>
   <div class="navigation-container">
     <a href="http://chanwutk.github.io/2024-04-21-grad-photos/{(number-1):02d}" class="p-btn">prev</a>
@@ -48,5 +55,5 @@ def page(number: int):
 """
 
 for i in range(19):
-    with open(f"./site/{i:02d}.html", "w") as f:
+    with open(f"./docs/{i:02d}.html", "w") as f:
         f.write(page(i))
